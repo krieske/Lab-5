847b2631bcfbdf818f8b87b21134f6a0442dd98b
kristen <thehostboy@gmail.com>
2018-07-22 21:19:45 -0400

    lab 5 basic specs, 1 challenge added: find highest factorial

---
+++ Lab5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            string yes;
+            do
+            {
+                Console.WriteLine("Welcome to the Factorial Calculator!");
+                Console.WriteLine("Please enter a whole number between 1 and 20:");
+                userInput = int.Parse(Console.ReadLine());
+
+                while (userInput < 0)
+                {
+                    Console.WriteLine("Please enter a positive number.");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+
+                long num = Convert.ToInt64(userInput);
+                long result = num;
+
+                for (int i = 1; i < userInput; i++)
+                {
+                    result = result * i;
+                }
+                Console.WriteLine(result);
+                Console.WriteLine("Do you want to go again? (yes/no)");
+                yes = Console.ReadLine();
+            }
+            while ((yes == "yes") || (yes == "y") || (yes == "Y") || (yes == "Yes") || (yes == "YES"));
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
